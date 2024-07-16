d6f8ecf14fcbc1795fd5454b4ebc52510ab80da7
mohamed_abdo <91019053+memobih@users.noreply.github.com>
2024-07-16 03:37:33 +0300

    Update showController.php

--- app/Http/Controllers/showController.php
+++ app/Http/Controllers/showController.php
@@ -10,9 +10,9 @@ use Cart;
 class showController extends Controller
 {
      public  function show( ){
-         $categories="aa";
-         $departments="";
-         $cartItems ="aa";
+         $categories=Category::all();
+         $departments=HomeCategories::select()->first();
+         $cartItems = Cart::instance('cart')->content();
         visitor()->visit(); // create a visit log
          return view('index' , compact('departments' , 'categories','cartItems' ));
      }
