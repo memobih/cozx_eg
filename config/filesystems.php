e3eee82cc7bde077e75ba5ab28b347af4963a307
mohamed_abdo <91019053+memobih@users.noreply.github.com>
2024-07-17 23:49:26 +0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,14 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+    'azure' => [
+            'driver'    => 'azure',
+            'name'      => env('AZURE_STORAGE_NAME'),
+            'key'       => env('AZURE_STORAGE_KEY'),
+            'container' => env('AZURE_STORAGE_CONTAINER'),
+            'url'       => env('AZURE_STORAGE_URL'),
+            'prefix'    => null,
+        ],
 
     ],
 
